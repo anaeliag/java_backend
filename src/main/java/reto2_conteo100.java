96a6733d67c732cf594b9632c8fbca722ec8dc35
Ana Garcia <sailor_ana@hotmail.com>
2024-04-09 14:47:48 -0600

    adding code changes for reto2

---
+++ src/main/java/reto2_conteo100.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class reto2_conteo100 {
+    public static void main(String[] args){
+        int valorfinal = 0;
+        for (int i=1;i<101;i++){
+            valorfinal=valorfinal+i;
+        }
+    System.out.println("El valor final es : " + valorfinal); 
+    }
+}
